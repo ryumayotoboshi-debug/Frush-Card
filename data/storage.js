8dbdcd0fd42b468b626234a1035ea90fed6c2ce0
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 17:05:59 +0900

    storage.js を更新

--- data/storage.js
+++ data/storage.js
@@ -1,9 +1,15 @@
-let cards = [];
+const KEY = "cards";
 
 export function getAllCards() {
-  return cards;
+  return JSON.parse(localStorage.getItem(KEY)) || [];
+}
+
+export function saveCards(cards) {
+  localStorage.setItem(KEY, JSON.stringify(cards));
 }
 
 export function addCard(card) {
+  const cards = getAllCards();
   cards.push(card);
+  saveCards(cards);
 }
