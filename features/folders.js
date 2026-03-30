27b0b9979281c8a05b4b2e22ffe9b3086555320f
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 20:38:38 +0900

    folders.js を更新

--- features/folders.js
+++ features/folders.js
@@ -1,33 +1,52 @@
 "use strict";
-
 import { load, save } from "../data/storage.js";
 
-export function getFolders(parentId) {
-  const data = load();
-  return data.folders.filter(f => f.parentId === parentId);
+export function getFolders() {
+  return load().folders;
 }
 
-export function addFolder(name, parentId) {
+export function addFolder(name, parentId = null) {
   const data = load();
-
   data.folders.push({
-    id: Date.now().toString(),
+    id: crypto.randomUUID(),
     name,
     parentId,
-    lastStudied: 0
+    lastStudied: null
   });
-
   save(data);
 }
 
-// ⭐ 追加：勉強した扱い
-export function touchFolder(folderId) {
+export function renameFolder(id, newName) {
   const data = load();
-
-  const folder = data.folders.find(f => f.id === folderId);
-  if (folder) {
-    folder.lastStudied = Date.now();
+  const target = data.folders.find(f => f.id === id);
+  if (target) {
+    target.name = newName;
+    save(data);
   }
+}
 
+export function deleteFolder(id) {
+  const data = load();
+  // フォルダとそのサブフォルダを削除
+  function removeRecursive(folderId) {
+    const children = data.folders.filter(f => f.parentId === folderId);
+    children.forEach(c => removeRecursive(c.id));
+    data.folders = data.folders.filter(f => f.id !== folderId);
+    data.words = data.words.filter(w => w.folderId !== folderId);
+  }
+  removeRecursive(id);
   save(data);
+}
+
+export function getFolderTree() {
+  const folders = getFolders();
+  function build(parentId = null) {
+    return folders
+      .filter(f => f.parentId === parentId)
+      .map(f => ({
+        ...f,
+        children: build(f.id)
+      }));
+  }
+  return build();
 }
