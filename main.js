53f78c0844a8ff118c0c9caf90bc5f4e04f0d9fc
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 16:14:49 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-/*全体の初期化、イベントの起点*/
 "use strict";
 
 import { setupForm } from "./ui/form.js";
-import { renderCards, renderQuiz } from "./ui/render.js";
+import { renderQuiz } from "./ui/render.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   setupForm();
-  renderCards();
-  renderQuiz(); // ←追加
+  renderQuiz();
 });
