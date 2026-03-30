b579ab54a455459f5346c72f2a7f1b8a05844073
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 11:13:21 +0900

    render.js を更新

--- ui/render.js
+++ ui/render.js
@@ -1,1 +1,31 @@
 //DOM生成専用、画面描画の専用担当
+"use strict";
+
+import { getCards } from "../features/cards.js";
+
+export function renderCards() {
+  const container = document.getElementById("cardList");
+  container.innerHTML = "";
+
+  const cards = getCards();
+
+  cards.forEach(card => {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <div class="front">${card.word}</div>
+      <div class="back hidden">
+        ${card.meaning}<br>
+        <small>${card.description}</small>
+      </div>
+    `;
+
+    div.addEventListener("click", () => {
+      div.querySelector(".front").classList.toggle("hidden");
+      div.querySelector(".back").classList.toggle("hidden");
+    });
+
+    container.appendChild(div);
+  });
+}
