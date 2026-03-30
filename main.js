8d1456ce5042abb0093a7088e4d75e4d6ed59942
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 20:48:20 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -2,11 +2,5 @@
 
 import { draw } from "./ui/render.js";
 
-// 🔥 実行確認ログ
-console.log("main.js 読み込み成功");
-
 // 初期描画
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM読み込み完了");
-  draw();
-});
+draw();
