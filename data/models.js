346ebe8489a11e2d7f14b509965eb6b42641e0a3
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 11:11:43 +0900

    models.js を更新

--- data/models.js
+++ data/models.js
@@ -1,1 +1,25 @@
 //データの型定義、データの“設計図”をまとめる
+"use strict";
+
+export function createCard({
+  id,
+  word,
+  meaning,
+  description = "",
+  tags = [],
+  folderId = null,
+  correct = 0,
+  wrong = 0
+}) {
+  return {
+    id,
+    word,
+    meaning,
+    description,
+    tags,
+    folderId,
+    correct,
+    wrong,
+    createdAt: Date.now()
+  };
+}
