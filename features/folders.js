316c6e34ca01dd8dc6b2886d0436141613a0dd67
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 17:25:12 +0900

    folders.js を更新

--- features/folders.js
+++ features/folders.js
@@ -1,1 +1,36 @@
-//フォルダ管理
+"use strict";
+import { load, save } from "../data/storage.js";
+import { createFolder } from "../data/models.js";
+
+export function getFolders() {
+  return load().folders;
+}
+
+export function addFolder(name, parentId = null) {
+  const data = load();
+  data.folders.push(createFolder(name, parentId));
+  save(data);
+}
+
+export function renameFolder(id, newName) {
+  const data = load();
+  const folder = data.folders.find(f => f.id === id);
+  if (folder) folder.name = newName;
+  save(data);
+}
+
+// 階層構造で取得
+export function getFolderTree() {
+  const folders = getFolders();
+
+  function build(parentId = null) {
+    return folders
+      .filter(f => f.parentId === parentId)
+      .map(f => ({
+        ...f,
+        children: build(f.id)
+      }));
+  }
+
+  return build(null);
+}
