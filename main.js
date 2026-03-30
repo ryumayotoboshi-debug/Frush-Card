37e2dcc6e14f1fc999fd2d633cd8be1ca5830c19
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 16:54:28 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-"use strict";
+import { setupUI } from "./ui/render.js";
 
-import { setupForm } from "./ui/form.js";
-import { renderQuiz } from "./ui/render.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  setupForm();
-  renderQuiz();
+// ★ DOM読み込み後に確実に実行
+document.addEventListener("DOMContentLoaded", () => {
+  setupUI();
 });
