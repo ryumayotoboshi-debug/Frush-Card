8c134e0e3c683134fb17d283897d4e786ad670b2
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 17:58:39 +0900

    folders.js を更新

--- features/folders.js
+++ features/folders.js
@@ -1,43 +1,55 @@
 "use strict";
-import { load, save } from "../data/storage.js";
 
-export function getFolders() {
-  return load().folders;
-}
+import { load, save } from "../storage.js";
 
+/**
+ * フォルダ追加
+ */
 export function addFolder(name, parentId = null) {
   const data = load();
 
-  data.folders.push({
+  const newFolder = {
     id: crypto.randomUUID(),
     name,
     parentId
-  });
+  };
+
+  data.folders.push(newFolder);
 
   save(data);
+
+  // 🔍 デバッグ（保存確認）
+  console.log("追加後データ:", load());
 }
 
+/**
+ * フォルダ一覧取得
+ */
+export function getFolders(parentId = null) {
+  const data = load();
+  return data.folders.filter(f => f.parentId === parentId);
+}
+
+/**
+ * フォルダリネーム
+ */
 export function renameFolder(id, newName) {
   const data = load();
-  const target = data.folders.find(f => f.id === id);
 
-  if (target) {
-    target.name = newName;
+  const folder = data.folders.find(f => f.id === id);
+  if (folder) {
+    folder.name = newName;
     save(data);
   }
 }
 
-export function getFolderTree() {
-  const folders = getFolders();
+/**
+ * フォルダ削除
+ */
+export function deleteFolder(id) {
+  const data = load();
 
-  function build(parentId = null) {
-    return folders
-      .filter(f => f.parentId === parentId)
-      .map(f => ({
-        ...f,
-        children: build(f.id)
-      }));
-  }
+  data.folders = data.folders.filter(f => f.id !== id);
 
-  return build();
+  save(data);
 }
