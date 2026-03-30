3a3e9ac99750e53563f6cbd96b164fe0a1b1b25a
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 21:26:36 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 import { drawFolderScreen } from "./ui/render.js";
 
-// iPhoneでもJSが動いているか確認
-alert("main.js 読み込み成功");
-
-drawFolderScreen();
+document.addEventListener("DOMContentLoaded", () => {
+  drawFolderScreen();
+});
