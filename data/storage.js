973ec03a1bf48d8cec34871434dbc1b230dc3481
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 11:12:01 +0900

    storage.js を更新

--- data/storage.js
+++ data/storage.js
@@ -1,1 +1,13 @@
 //localStorage操作をまとめる、データ保存の窓口
+"use strict";
+
+const KEY = "wordAppCards";
+
+export function saveCards(cards) {
+  localStorage.setItem(KEY, JSON.stringify(cards));
+}
+
+export function loadCards() {
+  const data = localStorage.getItem(KEY);
+  return data ? JSON.parse(data) : [];
+}
