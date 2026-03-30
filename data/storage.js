2810ec353c4ce71a906a1cc93bbecd90642de273
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 17:02:58 +0900

    storage.js を更新

--- data/storage.js
+++ data/storage.js
@@ -1,21 +1,9 @@
-"use strict";
+let cards = [];
 
-import { getSeedCards } from "./seed.js";
-
-const KEY = "wordAppCards";
-
-export function saveCards(cards) {
-  localStorage.setItem(KEY, JSON.stringify(cards));
+export function getAllCards() {
+  return cards;
 }
 
-export function loadCards() {
-  const data = localStorage.getItem(KEY);
-
-  if (!data) {
-    const seed = getSeedCards();
-    saveCards(seed);
-    return seed;
-  }
-
-  return JSON.parse(data);
+export function addCard(card) {
+  cards.push(card);
 }
