9dfcf0d046f46ab63781dee1511cc87c2a0932f1
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 11:12:43 +0900

    form.js を更新

--- ui/form.js
+++ ui/form.js
@@ -1,1 +1,22 @@
 //入力UIの制御
+"use strict";
+
+import { addCard } from "../features/cards.js";
+import { renderCards } from "./render.js";
+
+export function setupForm() {
+  const form = document.getElementById("cardForm");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const word = form.word.value;
+    const meaning = form.meaning.value;
+    const description = form.description.value;
+
+    addCard(word, meaning, description);
+    form.reset();
+
+    renderCards();
+  });
+}
