ae6b9d1302ba6594e5f880f61feda5fe826a413c
りゅま <ryumayotoboshi@gmail.com>
2026-03-30 11:12:23 +0900

    cards.js を更新

--- features/cards.js
+++ features/cards.js
@@ -1,1 +1,27 @@
 //カード表示、表裏切り替え
+"use strict";
+
+import { createCard } from "../data/models.js";
+import { saveCards, loadCards } from "../data/storage.js";
+
+let cards = loadCards();
+
+export function getCards() {
+  return cards;
+}
+
+export function addCard(word, meaning, description) {
+  const newCard = createCard({
+    id: crypto.randomUUID(),
+    word,
+    meaning,
+    description
+  });
+
+  cards.push(newCard);
+  saveCards(cards);
+}
+
+export function shuffleCards() {
+  return [...cards].sort(() => Math.random() - 0.5);
+}
