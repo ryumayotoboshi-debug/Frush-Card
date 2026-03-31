9b0ef5e0b90e67068562028766b28bba9cacaa1f
りゅま <ryumayotoboshi@gmail.com>
2026-03-31 17:53:01 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 "use strict";
 
 import { seed } from "./data/seed.js";
+
+// localStorage に古いデータがあれば削除して初期化
+localStorage.removeItem("wordAppData");
+seed();
 import { drawFolderScreen } from "./ui/render.js";
 
 // iPhoneのみでも必ず初期データをセット
