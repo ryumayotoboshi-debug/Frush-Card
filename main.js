91fcea5745b23ae1d5ddaf54771c2ce20312b5a2
りゅま <ryumayotoboshi@gmail.com>
2026-04-08 12:30:01 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import { seed } from "./data/seed.js";
 import { drawFolderScreen } from "./ui/render.js";
 
 console.log("🔥 main.js 読み込み");
@@ -7,5 +8,7 @@ console.log("🔥 main.js 読み込み");
 document.addEventListener("DOMContentLoaded", () => {
   console.log("✅ DOM 準備完了");
 
-  drawFolderScreen();
+  seed(); // ← データ生成（重要）
+
+  drawFolderScreen(); // ← 描画
 });
